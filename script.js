93e2f9d81e2ab69af15c58004eac73bafc0c9f22
ALFAJR <165387645+ALFAJ14287@users.noreply.github.com>
2024-12-16 11:49:49 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+function submitForm() {
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbzCNApHpe2jO62LzKPulAFuX6lZjHP20a21N17D8nucD-Xl5FlzmhD2UANPc8-6v7fS/exec'; // Replace with your Google Apps Script URL
+  const form = document.getElementById('registrationForm');
 
+  const formData = new FormData(form);
+
+  fetch(scriptURL, { method: 'POST', body: formData })
+    .then(response => {
+      alert('Form submitted successfully!');
+      form.reset();
+    })
+    .catch(error => {
+      console.error('Error!', error.message);
+      alert('There was an error submitting the form.');
+    });
+}
